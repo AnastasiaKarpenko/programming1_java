1d1754645a77f5bd4a17efe583ed9bb134410507
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-01 15:53:32 +0300

    solved task 73

---
+++ week4-072.3.MoneyTransfers/src/Person.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anastasia
+ */
+public class Person {
+    private String name;
+    private int age;
+    
+    public Person (String initialName) {
+        this.name = initialName;
+        this.age = 0;
+    }
+    
+    public void printPerson () {
+        System.out.println(this.name + " , age " + this.age + " years");
+    }
+    
+}
