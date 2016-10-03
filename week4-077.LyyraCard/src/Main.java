3d7b634e5e45b2f4b59f634a0c6e9d858f0ea0dd
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-03 16:59:12 +0300

    solved 77.6

--- week4-077.LyyraCard/src/Main.java
+++ week4-077.LyyraCard/src/Main.java
@@ -4,10 +4,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        LyyraCard card = new LyyraCard(10);
-        System.out.println("Pekka: " + card);
-        card.loadMoney(-15);
-        System.out.println("Pekka: " + card);
+        LyyraCard card1 = new LyyraCard(20);
+        LyyraCard card2 = new LyyraCard(30);
+        
+        card1.payGourmet();
+        card2.payEconomical();
+        
+        System.out.println("Pekka: " + card1);
+        System.out.println("Brian: " + card2);
+        
+        card1.loadMoney(20);
+        card2.payGourmet();
+        
+        System.out.println("Pekka: " + card1);
+        System.out.println("Brian: " + card2);
+        
+        card1.payEconomical();
+        card1.payEconomical();
+        
+        card2.loadMoney(50);
+        
+        System.out.println("Pekka: " + card1);
+        System.out.println("Brian: " + card2);
+ 
 
 
     }
