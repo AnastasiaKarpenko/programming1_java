1d372236b8614534a7ae254340de8b2fb7d3f18a
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-03 16:50:31 +0300

    solved tasks 77.1-77.4

--- week4-077.LyyraCard/src/Main.java
+++ week4-077.LyyraCard/src/Main.java
@@ -4,8 +4,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // add here code that tests LyraCard. However before doing 77.6 remove the
-        // other code 
+        LyyraCard card = new LyyraCard(10);
+        System.out.println(card);
+
+        card.loadMoney(15);
+        System.out.println(card);
+
+        card.loadMoney(10);
+        System.out.println(card);
+
+        card.loadMoney(200);
+        System.out.println(card);
+
 
     }
 }
