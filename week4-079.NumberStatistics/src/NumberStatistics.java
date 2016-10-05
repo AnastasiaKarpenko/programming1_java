48037a823f97f71e01e8508d4cf5c6d496a15650
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-05 15:56:20 +0300

    task 80 solved

--- week4-079.NumberStatistics/src/NumberStatistics.java
+++ week4-079.NumberStatistics/src/NumberStatistics.java
@@ -1,3 +1,19 @@
 
 public class NumberStatistics {
+    private int amountOfNumber;
+    public int counter;
+    
+    public NumberStatistics() {
+        this.amountOfNumber = 0;
+    }
+     public void addNumber(int number) {
+       
+       this.amountOfNumber += number;
+       counter++;
+    }
+     
+     public int amountOfNumbers() {
+         return counter;
+        
+    }
 }
