7cad366d8dcb3653e252234153376aad3ee334a1
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-17 12:19:18 +0300

    90.1 added class team

---
+++ week5-090.TeamAndPlayers/src/Team.java
@@ -0,0 +1,12 @@
+
+public class Team {
+    private String name;
+    
+    public Team (String teamName) {
+        this.name = teamName;
+    }
+    
+    public String getName () {
+        return this.name; 
+    }
+}
