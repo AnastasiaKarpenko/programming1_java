e6b46108fd3b568becad8e6fd0e3e75a001aea99
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-17 16:39:03 +0300

    90.3

--- week5-090.TeamAndPlayers/src/Team.java
+++ week5-090.TeamAndPlayers/src/Team.java
@@ -1,12 +1,30 @@
 
+import java.util.ArrayList;
+
+
+
 public class Team {
     private String name;
+    private ArrayList<Player> players;
     
     public Team (String teamName) {
         this.name = teamName;
+        players = new ArrayList();
+       
     }
     
     public String getName () {
         return this.name; 
+    }
+    
+    public void addPlayer(Player newPlayer) {
+        this.players.add(newPlayer);       
+       
+    }
+    
+    public void printPlayers () {
+        for (Player player : players) {
+            System.out.println(player);
+        }
     }
 }
