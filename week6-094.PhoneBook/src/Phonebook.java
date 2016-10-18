c780dd8deeeb27eebffc7298ad21109230fef3f1
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-18 15:19:15 +0300

    94.2

--- week6-094.PhoneBook/src/Phonebook.java
+++ week6-094.PhoneBook/src/Phonebook.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,23 @@
  * @author anastasia
  */
 public class Phonebook {
+    private ArrayList<Person> phonebook;
+    
+    public Phonebook () {
+        this.phonebook = new ArrayList<Person>();
+    }
+    
+    public void add(String name, String number) {
+        Person newContact = new Person(name, number); 
+        phonebook.add(newContact);
+         
+    }
+
+    public void printAll() {
+        for (Person person : phonebook) {
+            System.out.println(person);
+        }
+        
+    }
     
 }
