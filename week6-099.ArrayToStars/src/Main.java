129b568dee96b2163e4395244a62df13fe7e3580
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-20 17:10:19 +0300

    99

--- week6-099.ArrayToStars/src/Main.java
+++ week6-099.ArrayToStars/src/Main.java
@@ -8,6 +8,11 @@ public class Main {
     }
 
     public static void printArrayAsStars(int[] array) {
-        // write code here
+       for (int i : array) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
     }
 }
