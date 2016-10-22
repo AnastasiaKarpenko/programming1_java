7a99ac97dc50b15ee4b6c8aae29ccd28df89c2cb
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-22 18:24:19 +0300

    101.1

--- week6-101.Library/src/Main.java
+++ week6-101.Library/src/Main.java
@@ -1,5 +1,13 @@
+
 public class Main {
+
     public static void main(String[] args) {
         // test your program here
+        Book cheese = new Book("Cheese Problems Solved", "Woodhead Publishing", 2007);
+        System.out.println(cheese.title());
+        System.out.println(cheese.publisher());
+        System.out.println(cheese.year());
+
+        System.out.println(cheese);
     }
 }
