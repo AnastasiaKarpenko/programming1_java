e86d808f3d92babb82ae03286b5267a0da5f9744
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2016-10-25 22:02:07 +0300

    104.1-2-3

--- week6-104.Sorting/src/Main.java
+++ week6-104.Sorting/src/Main.java
@@ -1,7 +1,49 @@
 
 public class Main {
+
     public static void main(String[] args) {
-        // write testcode here
+        int[] values = {-1, 6, 9, 8, 12};
+        System.out.println(indexOfTheSmallestStartingFrom(values, 1));
+        System.out.println(indexOfTheSmallestStartingFrom(values, 2));
+        System.out.println(indexOfTheSmallestStartingFrom(values, 4));
+
+    }
+
+    public static int smallest(int[] array) {
+
+        int smallest = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+            }
+        }
+        return smallest;
+    }
+
+    public static int indexOfTheSmallest(int[] array) {
+        int smallest = array[0];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+                index = i;
+            }
+        }
+        return index;
+
+    }
+
+    public static int indexOfTheSmallestStartingFrom(int[] array, int index) {
+    // write the code here
+        int smallest = array[index];
+        int indexOfSmallest = index;
+        for (int i = index; i < array.length; i++) {
+            if (array[i] < smallest) {
+                smallest = array[i];
+                indexOfSmallest = i;
+            }
+        }
+        return indexOfSmallest;
     }
 
 }
